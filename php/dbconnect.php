a1d2144c21561a31f6a481e4d3d44741404f0a44
mhttekin <cb221086@qmul.ac.uk>
2024-04-15 14:25:52 +0000

    apache config

---
+++ php/dbconnect.php
@@ -0,0 +1,15 @@
+
+<?php
+$host = 'localhost';
+$dbname = 'blogDB';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection successfully";  // Fixed a typo here as well from 'successfuly' to 'successfully'
+} catch (PDOException $e) {  // Specify the exception type and variable
+    die("Could not connect to the database $dbname: " . $e->getMessage());
+}
+?>
--- php/login.php
+++ php/login.php
@@ -1,11 +1,22 @@
 <?php
 session_start();
+require 'dbconnect.php';
 
-if(isset($_POST['uname']) && isset($_POST['psw'])) {
-   $username = $_POST['uname'];
-   $password = $_POST['psw'];
-    
-   
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+$sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$username, $password]);
+$user = $stmt->fetch();
+
+if($user) {
+  $_SESSION['user_id'] = $user['id'];
+  header('Location: blogpage.php');
+  exit();
+
+} else {
+  echo "Invalid username or password!";
 }
 
+?>
